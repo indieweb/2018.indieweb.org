766452654831e1990183af00cf43119046b0a730
Marty McGuire <schmartissimo@gmail.com>
2017-12-06 14:49:33 -0500

    Initial 2018.indieweb.org/baltimore

---
+++ templates/baltimore/sponsors.php
@@ -0,0 +1,16 @@
+<p>Special thanks to our sponsors who make it possible to provide food and space for the IndieWeb Summit</p>
+
+<ul>
+  <li>
+    <span class="logo">
+      <a href="https://martymcgui.re/podcasts/indieweb/"><img src="https://indieweb.org/sponsor-logos/full/this-week-audio-edition-logo.png" style="width: 130px;"></a>
+    </span>
+    <span class="description"><a href="https://martymcgui.re/podcasts/indieweb/">This Week in the IndieWeb Audio Edition</a> is a weekly podcast summarizing activity in the IndieWeb community based on the <a href="http://indieweb.org/this-week">weekly newsletter</a>.</span>
+  </li>
+</ul>
+
+
+<h4 class="ui header">How to Sponsor</h4>
+<p>We would be thrilled to list you as a sponsor! See our sponsorship prospectus for details, or <a href="https://opencollective.com/indieweb">become a monthly supporter</a>.</p>
+<a class="ui button" href="https://indiewebcamp.com/images/2/2d/indiewebcamp-sponsorship-prospectus.pdf">Download Sponsorship Prospectus (PDF)</a>
+
