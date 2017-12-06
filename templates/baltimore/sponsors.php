9d46f4bc185771c1e38912ec754895aea674fed6
Marty McGuire <schmartissimo@gmail.com>
2017-12-06 15:49:24 -0500

    Add Digital Harbor Foundation sponsor info

--- templates/baltimore/sponsors.php
+++ templates/baltimore/sponsors.php
@@ -3,6 +3,12 @@
 <ul>
   <li>
     <span class="logo">
+      <a href="https://www.digitalharbor.org/"><img src="https://indieweb.org/sponsor-logos/full/digital-harbor-foundation-logo.png" style="width: 130px;"></a>
+    </span>
+    <span class="description"><a href="https://www.digitalharbor.org/">The Digital Harbor Foundation</a> is dedicated to fostering learning, creativity, productivity, and community through education. In 2013 we transformed a closed-down rec center in Baltimore City into a vibrant Tech Center for youth.</span>
+  </li>
+  <li>
+    <span class="logo">
       <a href="https://martymcgui.re/podcasts/indieweb/"><img src="https://indieweb.org/sponsor-logos/full/this-week-audio-edition-logo.png" style="width: 130px;"></a>
     </span>
     <span class="description"><a href="https://martymcgui.re/podcasts/indieweb/">This Week in the IndieWeb Audio Edition</a> is a weekly podcast summarizing activity in the IndieWeb community based on the <a href="http://indieweb.org/this-week">weekly newsletter</a>.</span>
