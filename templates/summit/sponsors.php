748fa964c0c23ce40db401d6cfa91c6941b4725b
Aaron Parecki <aaron@parecki.com>
2018-04-09 10:55:31 -0700

    add sponsors

--- templates/summit/sponsors.php
+++ templates/summit/sponsors.php
@@ -1,34 +1,41 @@
 <p>Special thanks to our sponsors who make it possible to provide food and space for the IndieWeb Summit</p>
 
 <ul>
+
   <li>
     <span class="logo">
-      <a href="https://mozilla.org"><img src="https://indiewebcamp.com/sponsor-logos/full/mozilla-logo.png" style="width: 130px;"></a>
+      <a href="https://opencollective.com/indieweb"><img src="https://indieweb.org/sponsor-logos/full/opencollective-logo.svg" style="width: 70px;"></a>
     </span>
-    <span class="description"><a href="https://mozilla.org">Mozilla</a> is a non-profit organization helping to keep the Internet healthy, open and accessible to all.</span>
+    <span class="description">Thanks to our backers on <a href="https://opencollective.com/indieweb">Open Collective</a> for their continued support!</span>
   </li>
+
   <li>
     <span class="logo">
-      <a href="http://brid.gy/"><img src="https://indiewebcamp.com/sponsor-logos/full/bridgy-logo.png" style="width: 140px;"></a>
+      <a href="https://developer.okta.com/"><img src="https://indieweb.org/sponsor-logos/full/okta-logo.svg" style="width: 120px;"></a>
+    </span>
+    <span class="description"><a href="https://developer.okta.com">Okta</a> provides an API that seamlessly handles multi-factor authentication and user registration for your applications.</span>
+  </li>
+
+  <li>
+    <span class="logo">
+      <a href="http://brid.gy/"><img src="https://indieweb.org/sponsor-logos/full/bridgy-logo.png" style="width: 140px;"></a>
     </span>
     <span class="description"><a href="http://brid.gy/">Bridgy</a> is an open source project and proxy that implements backfeed as a service. Bridgy sends webmentions for comments, likes, etc. on Facebook, Twitter, Google+, and Instagram.</span>
   </li>
+
+<!--
   <li>
     <span class="logo">
-      <a href="https://donutjs.club/"><img src="https://indiewebcamp.com/sponsor-logos/full/donutjs.png" style="width: 90px"></a>
+      <a href="https://mozilla.org"><img src="https://indieweb.org/sponsor-logos/full/mozilla-logo.png" style="width: 130px;"></a>
     </span>
-    <span class="description"><a href="https://donutjs.club/">Donut.js</a> is a monthly meetup bringing together the love of code and the deliciousness of donuts.</span>
+    <span class="description"><a href="https://mozilla.org">Mozilla</a> is a non-profit organization helping to keep the Internet healthy, open and accessible to all.</span>
   </li>
-  <li>
-    <span class="logo">
-      <a href="https://csvconf.com/"><img src="https://indiewebcamp.com/sponsor-logos/full/csvconf.png" style="width: 140px"></a>
-    </span>
-    <span class="description"><a href="https://csvconf.com/">CSV Conf</a> is a conference featuring stories about data sharing and data analysis from science, journalism, government, and open source.</span>
-  </li>
+-->
+
 </ul>
 
 
 <h4 class="ui header">How to Sponsor</h4>
 <p>We would be thrilled to list you as a sponsor! See our sponsorship prospectus for details, or <a href="https://opencollective.com/indieweb">become a monthly supporter</a>.</p>
-<a class="ui button" href="https://indiewebcamp.com/images/2/2d/indiewebcamp-sponsorship-prospectus.pdf">Download Sponsorship Prospectus (PDF)</a>
+<a class="ui button" href="https://indieweb.org/images/2/2d/indiewebcamp-sponsorship-prospectus.pdf">Download Sponsorship Prospectus (PDF)</a>
 
