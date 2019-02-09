a0b6d21f4cd64539034facd7f814cca3bf207b3c
Aaron Parecki <aaron@parecki.com>
2019-02-09 15:50:04 -0800

    comment out registration section

--- public/index.php
+++ public/index.php
@@ -117,7 +117,7 @@ include(dirname(__FILE__).'/../lib/rsvps.php');
 
   </div>
 
-
+  <!--
   <div class="ui vertical stripe segment" id="register">
     <div class="ui text container">
       <h3 class="ui header">Register</h3>
@@ -128,7 +128,7 @@ include(dirname(__FILE__).'/../lib/rsvps.php');
 
     </div>
   </div>
-
+  -->
 
   <div class="ui vertical stripe segment" id="rsvps">
     <div class="ui text container">
